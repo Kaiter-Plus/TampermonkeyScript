a95c0b1d1dfe22a2efd553b8feab9ea4ffcc3607
Kaiter-Plus <738406369@qq.com>
2022-03-28 19:32:50 +0800

    reafctor: change hotkey

--- Translate/Translate_only_chinese.js
+++ Translate/Translate_only_chinese.js
@@ -3,7 +3,7 @@
 // @author       Kaiter-Plus
 // @namespace    https://gitee.com/Kaiter-Plus/TampermonkeyScript/tree/master/Translate/Translate_only_chinese.js
 // @description  给每个非中文的网页右下角（可以调整到左下角）添加一个google翻译图标，该版本为中文翻译版本，只把外语翻译为中文
-// @version      0.20
+// @version      0.21
 // @license      BSD-3-Clause
 // @require      https://greasyfork.org/scripts/441796-google-translate-supported-languages/code/Google%20Translate%20Supported%20Languages.js?version=1030327
 // @include      *://*
@@ -56,6 +56,7 @@
 // @note         2022/03/20 修复开启显示【更好的翻译建议】无法自动消失的 bug
 // @note         2022/03/23 添加【翻译】快捷键：Ctrl + Alt + T, 【还原】快捷键：Ctrl + Alt + R
 // @note         2022/03/27 修改【翻译】快捷键：Alt + T, 【还原】快捷键：Alt + R
+// @note         2022/03/28 修改【翻译】快捷键：Ctrl + Shift + Alt + T, 【还原】快捷键：Ctrl + Shift + Alt + R
 // ==/UserScript==
 
 ;(function () {
@@ -376,13 +377,13 @@
                 }
                 // 添加翻译快捷键
                 document.addEventListener('keydown', (e) => {
-                  if (e.altKey && e.key.toLowerCase() === 't') {
+                  if (e.ctrlKey && e.shiftKey && e.altKey && e.key.toLowerCase() === 't') {
                     translateButton.click()
                   }
                 })
                 // 添加还原快捷键
                 document.addEventListener('keydown', (e) => {
-                  if (e.altKey && e.key.toLowerCase() === 'r') {
+                  if (e.ctrlKey && e.shiftKey && e.altKey && e.key.toLowerCase() === 'r') {
                     recoverPage.click()
                   }
                 })
