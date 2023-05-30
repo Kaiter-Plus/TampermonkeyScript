c3946fd07ed5b9bf10af77d18bfb9d8c0ea5d007
Kaiter-Plus <738406369@qq.com>
2023-05-30 14:15:18 +0800

    refactor(douyin-auto-like): 代码重构

--- douyin=auto-like/index.js
+++ douyin=auto-like/index.js
@@ -3,7 +3,7 @@
 // @namespace   https://gitee.com/Kaiter-Plus/TampermonkeyScript/tree/master/douyin=auto-like
 // @author      Kaiter-Plus
 // @description 网页版抖音直播添加自动点赞功能
-// @version     0.02
+// @version     0.03
 // @license     BSD-3-Clause
 // @match       *://live.douyin.com/*
 // @icon        https://lf1-cdn-tos.bytegoofy.com/goofy/ies/douyin_web/public/favicon.ico
@@ -12,36 +12,51 @@
 // ==/UserScript==
 
 ;(() => {
+  // 多久点击一次
+  const CLICK_DURATION = 150
+  // 点击层的类名
   const CLICK_MODAL_CLASS = '.Zs4Pv2bD'
   // 点击事件层
   let clickModal = document.querySelector(CLICK_MODAL_CLASS)
-  // 创建一个点击元素
-  const clickElement = document.createElement('div')
-  clickElement.style.position = 'absolute'
-  // 点击元素是否已经添加，防止重复添加
-  let isAppend = false
+  // 定时器
+  let timer = null
+  // 记录上一次执行的时间
+  let prevTImestamp = 0
 
   // 循环点击
-  let timer = setInterval(() => {
-    if (clickModal) {
-      clearLikeIcon()
-      clickElement.style.left = `${Math.random() * clickModal.clientWidth}px`
-      clickElement.style.top = `${Math.random() * clickModal.clientHeight}px`
-      if (!isAppend) {
-        clickModal.appendChild(clickElement)
-        isAppend = true
+  function autoClick(timestamp) {
+    const duration = timestamp - prevTImestamp
+    if (duration >= CLICK_DURATION) {
+      if (clickModal) {
+        clearLikeIcon()
+        // 获取元素的坐标
+        const rect = clickModal.getBoundingClientRect()
+        const x = rect.bottom - Math.random() * 200
+        const y = rect.right - Math.random() * 200
+        // 创建模拟点击事件
+        const clickEvent = new MouseEvent('click', {
+          view: window,
+          bubbles: true,
+          cancelable: true,
+          clientX: x,
+          clientY: y
+        })
+        // 触发模拟点击事件
+        clickModal.dispatchEvent(clickEvent)
+      } else {
+        clickModal = document.querySelector(CLICK_MODAL_CLASS)
       }
-      clickElement.click()
-    } else {
-      clickModal = document.querySelector(CLICK_MODAL_CLASS)
+      prevTImestamp = timestamp
     }
-  }, 300)
+    timer = requestAnimationFrame(autoClick)
+  }
+
+  autoClick(0)
 
   // 自动清理点赞之后留下的元素(抖音前端工程师长点心吧！无限添加元素，想卡死用户是吧！不会复用元素？)
   function clearLikeIcon() {
     if (clickModal.children.length > 30) {
       clickModal.textContent = ''
-      isAppend = false
     }
   }
 })()
