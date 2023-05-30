7e129383419e739aa19ae5ef4d03c4d4f86c938f
Kaiter-Plus <738406369@qq.com>
2023-05-30 13:37:41 +0800

    feat(douyin-auto-like):解决官方没有优化，点赞太多，元素不会自动清理导致浏览器卡顿的问题

--- douyin=auto-like/index.js
+++ douyin=auto-like/index.js
@@ -3,7 +3,7 @@
 // @namespace   https://gitee.com/Kaiter-Plus/TampermonkeyScript/tree/master/douyin=auto-like
 // @author      Kaiter-Plus
 // @description 网页版抖音直播添加自动点赞功能
-// @version     0.01
+// @version     0.02
 // @license     BSD-3-Clause
 // @match       *://live.douyin.com/*
 // @icon        https://lf1-cdn-tos.bytegoofy.com/goofy/ies/douyin_web/public/favicon.ico
@@ -12,8 +12,9 @@
 // ==/UserScript==
 
 ;(() => {
+  const CLICK_MODAL_CLASS = '.Zs4Pv2bD'
   // 点击事件层
-  let clickModal = document.querySelector('.Zs4Pv2bD')
+  let clickModal = document.querySelector(CLICK_MODAL_CLASS)
   // 创建一个点击元素
   const clickElement = document.createElement('div')
   clickElement.style.position = 'absolute'
@@ -23,6 +24,7 @@
   // 循环点击
   let timer = setInterval(() => {
     if (clickModal) {
+      clearLikeIcon()
       clickElement.style.left = `${Math.random() * clickModal.clientWidth}px`
       clickElement.style.top = `${Math.random() * clickModal.clientHeight}px`
       if (!isAppend) {
@@ -31,7 +33,15 @@
       }
       clickElement.click()
     } else {
-      clickModal = document.querySelector('.Zs4Pv2bD')
+      clickModal = document.querySelector(CLICK_MODAL_CLASS)
     }
   }, 300)
+
+  // 自动清理点赞之后留下的元素(抖音前端工程师长点心吧！无限添加元素，想卡死用户是吧！不会复用元素？)
+  function clearLikeIcon() {
+    if (clickModal.children.length > 30) {
+      clickModal.textContent = ''
+      isAppend = false
+    }
+  }
 })()
