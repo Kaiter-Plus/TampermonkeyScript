fae79472ba2a19c8ae14ecab5415cae4b9ce3bf2
Kaiter-Plus <bundows@foxmail.com>
2024-12-19 22:36:23 +0800

    feat: 添加抖音直播自动点赞脚本

---
+++ packages/douyin-live-auto-click-like/README.md
@@ -0,0 +1,35 @@
+## 前言
+
+### 支持作者
+
+![支付宝](https://greasyfork.s3.us-east-2.amazonaws.com/lg94h87tgo1nuetf5n2fuhvloa2s)
+<img src="https://greasyfork.s3.us-east-2.amazonaws.com/svemz9wsqdw7ym25wnwarww7jwit" alt="微信" title="微信" width="200" />
+
+### 所有脚本开源地址，欢迎 star ⭐
+
+- gitee：[https://gitee.com/Kaiter-Plus/TampermonkeyScript](https://gitee.com/Kaiter-Plus/TampermonkeyScript)
+- github：[https://github.com/Kaiter-Plus/TampermonkeyScript](https://github.com/Kaiter-Plus/TampermonkeyScript)
+
+# <span style="color:red">!!注意!!</span>
+
+- <span style="color:red">本脚本仅供学习交流使用，请勿用于非法用途！</span>
+- <span style="color:red">使用本脚本用于非法用途后果自负！</span>
+- <span style="color:red">请于下载后 24 小时内删除！</span>
+- <span style="color:red">作者本人不负任何法律责任！</span>
+
+## 1 脚本介绍
+
+最近网页版的抖音直播终于添加了点赞功能，手动点赞太伤鼠标了。所以写了该脚本实现自动点赞。
+
+## 更新日志
+
+- 2023/11/27 修复【手速太快】提示移除失败的 bug
+- 2023/11/25 修复脚本失效的问题
+- 2023/10/07 修复【手速太快】提示移除失败的 bug
+- 2023/06/09 修复功能关闭之后刷新页面还是自动点赞的 bug
+- 2023/06/02 添加脚本开关菜单
+- 2023/06/01 修复 TamperMonkey 无法使用的 bug
+- 2023/05/31 优化显示，在左上角显示点赞效果
+- 2023/05/30 代码优化，移除【您手速太快了，请休息一下】提示
+- 2023/05/29 解决官方没有优化，点赞太多，元素不会自动清理导致浏览器卡顿的问题
+- 2023/05/28 抖音直播自动点赞功能
---
+++ packages/douyin-live-auto-click-like/src/index.ts
@@ -0,0 +1,189 @@
+// 多久点击一次
+const CLICK_DURATION: number = 300
+// 点击层的类名
+const CLICK_MODAL_CLASS: string = '.LO5TGkc0'
+// 点击事件层
+let clickModal = document.querySelector(CLICK_MODAL_CLASS)
+// 定时器
+let timer: number | null = null
+// 重新恢复点赞的定时器
+let restartTimer: number | null = null
+// 重新开始点赞的时间
+let restartTimestamp: number = GM_getValue('restartTimestamp') ? Number(GM_getValue('restartTimestamp')) : 0
+
+// 循环点击
+let prevTImestamp: number = 0 // 记录上一次执行的时间
+function autoClick(timestamp: number) {
+  const duration = timestamp - prevTImestamp
+  if (duration >= CLICK_DURATION + Math.random() * 100 - 50) {
+    if (clickModal) {
+      clearLikeIcon()
+      // 获取元素的坐标
+      const rect = clickModal.getBoundingClientRect()
+      const x = rect.top + Math.random() * 50 + 50
+      const y = rect.left + Math.random() * 50 + 50
+      // 创建模拟点击事件
+      const clickEvent = new MouseEvent('click', {
+        view: unsafeWindow,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: y
+      })
+      // 触发模拟点击事件
+      clickModal.dispatchEvent(clickEvent)
+    } else {
+      clickModal = document.querySelector(CLICK_MODAL_CLASS)
+    }
+    prevTImestamp = timestamp
+  }
+  timer = requestAnimationFrame(autoClick)
+}
+
+// 自动清理点赞之后留下的元素(抖音前端工程师长点心吧！无限添加元素，想卡死用户是吧！不会复用元素？)
+function clearLikeIcon() {
+  if (clickModal!.children.length > 30) {
+    clickModal!.textContent = ''
+  }
+}
+
+// 如果已经提示【手速太快】，暂停 1 分钟再重新开始
+let prevRestartTimestamp = 0
+function restart(timestamp: number) {
+  cancelAnimationFrame(timer!)
+  const duration = timestamp - prevRestartTimestamp
+  if (duration >= CLICK_DURATION) {
+    const currentTimestamp = +new Date()
+    if (currentTimestamp >= restartTimestamp) {
+      if (GM_getValue('switch')) {
+        timer = requestAnimationFrame(autoClick)
+        restartTimer = null
+        return
+      }
+    }
+    prevRestartTimestamp = timestamp
+  }
+  restartTimer = requestAnimationFrame(restart)
+}
+
+// 移除【您手速太快了，请休息一下】提示
+let prevRemoveTipTimestamp = 0
+function removeTip(timestamp: number) {
+  const duration = timestamp - prevRemoveTipTimestamp
+  if (duration >= CLICK_DURATION) {
+    const reg = /.*手速太快.*/
+    const toastContainer = document.getElementById('toastContainer')
+    if (toastContainer) {
+      if (toastContainer.children[0]) {
+        toastContainer.textContent = ``
+        if (!restartTimer) {
+          const nextTimestamp = +new Date() + 60000
+          restartTimestamp = nextTimestamp - restartTimestamp > 60000 ? nextTimestamp : restartTimestamp
+          GM_setValue('restartTimestamp', restartTimestamp)
+          restart(timestamp)
+        }
+      }
+    }
+    const toast = document.querySelector('[data-e2e="toast"]')
+    if (toast && reg.test(toast.textContent!)) {
+      toast.parentNode?.removeChild(toast)
+      if (!restartTimer) {
+        const nextTimestamp = +new Date() + 60000
+        restartTimestamp = nextTimestamp - restartTimestamp > 60000 ? nextTimestamp : restartTimestamp
+        GM_setValue('restartTimestamp', restartTimestamp)
+        restart(timestamp)
+      }
+    }
+    prevRemoveTipTimestamp = timestamp
+  }
+  requestAnimationFrame(removeTip)
+}
+
+interface Tip {
+  open: string
+  close: string
+}
+
+interface MenuItem {
+  key: string
+  name: string
+  value: boolean
+  showNotification: boolean
+  tip: Tip
+  click: () => void
+}
+
+// 菜单
+const menu: MenuItem[] = [
+  {
+    key: 'switch',
+    name: '自动点赞',
+    value: true,
+    showNotification: true,
+    tip: {
+      open: '✅',
+      close: '❌'
+    },
+    click: switchFn
+  }
+]
+
+// 保存已注册的菜单
+const menuRegister: number[] = []
+
+// 配置默认菜单
+menu.forEach(v => {
+  if (GM_getValue(v.key) === undefined || GM_getValue(v.key) === null) GM_setValue(v.key, v.value)
+})
+
+// 注册菜单
+function registerMenuCommand() {
+  if (menuRegister.length === menu.length) {
+    menuRegister.forEach(v => {
+      GM_unregisterMenuCommand(v)
+    })
+  }
+  menu.forEach((v, i) => {
+    v.value = GM_getValue(v.key)
+    menuRegister[i] = GM_registerMenuCommand(`${v.value ? v.tip.open : v.tip.close} ${v.name}`, () => {
+      menuSwitch(v)
+    })
+  })
+}
+
+// 切换菜单
+function menuSwitch(item: MenuItem) {
+  // 设置数据
+  item.value = !item.value
+  GM_setValue(item.key, item.value)
+  // 系统通知
+  if (item.showNotification) {
+    GM_notification({
+      text: `已${item.value ? item.tip.open : item.tip.close}[${item.name}] 功能`,
+      title: '抖音自动点赞',
+      timeout: 1000
+    })
+  }
+  // 如果有点击事件，执行
+  if (item.click) item.click()
+  // 重新注册
+  registerMenuCommand()
+}
+
+// 切换开关
+function switchFn() {
+  if (GM_getValue('switch')) timer = requestAnimationFrame(autoClick)
+  else cancelAnimationFrame(timer!)
+}
+
+function init() {
+  clickModal = document.querySelector(CLICK_MODAL_CLASS)
+  registerMenuCommand()
+  requestAnimationFrame(removeTip)
+  if (GM_getValue('switch')) timer = requestAnimationFrame(autoClick)
+}
+
+// 延迟 3 秒开始，抖音需要获取登录数据
+setTimeout(() => {
+  init()
+}, 3000)
